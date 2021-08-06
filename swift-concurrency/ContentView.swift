4b1b1f706431e83c68ecb25ff6694e88f854d27a
vivalalova <vivalalova0@gmail.com>
2021-08-06 18:22:22 +0800

    model

--- swift-concurrency/ContentView.swift
+++ swift-concurrency/ContentView.swift
@@ -9,30 +9,6 @@ import Combine
 import SwiftUI
 
 struct ContentView: View {
-    class ViewModel: ObservableObject {
-        @Published var users: [User] = []
-
-        func fetchUsers() {
-            Task(priority: .medium) {
-                self.users = await Fetch.request(from: "https://jsonplaceholder.typicode.com/users")
-            }
-        }
-
-        private func fetch<T: Codable>(from urlString: String) async -> [T] {
-            guard let url = URL(string: urlString) else {
-                return []
-            }
-
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                let results = try JSONDecoder().decode([T].self, from: data)
-                return results
-            } catch {
-                return []
-            }
-        }
-    }
-
     @StateObject var model = ViewModel()
 
     var body: some View {
@@ -58,6 +34,32 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var users: [User] = []
+
+        func fetchUsers() {
+            Task(priority: .medium) {
+                self.users = await Fetch.request(from: "https://jsonplaceholder.typicode.com/users")
+            }
+        }
+
+        private func fetch<T: Codable>(from urlString: String) async -> [T] {
+            guard let url = URL(string: urlString) else {
+                return []
+            }
+
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let results = try JSONDecoder().decode([T].self, from: data)
+                return results
+            } catch {
+                return []
+            }
+        }
     }
 }
 
