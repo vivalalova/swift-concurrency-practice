80d5dd0bcec4b4cba0c5174e0ad1d95f0a0ce40c
vivalalova <vivalalova0@gmail.com>
2021-08-06 16:01:43 +0800

    Initial Commit

---
+++ swift-concurrency/swift_concurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_concurrencyApp.swift
+//  swift-concurrency
+//
+//  Created by Lova on 2021/8/6.
+//
+
+import SwiftUI
+
+@main
+struct swift_concurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
